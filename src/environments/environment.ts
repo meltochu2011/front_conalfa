d944310c20b2dad3b2d69e0f79f77f793fcc8115
Melvin <meltochu2011@gmail.com>
2025-11-09 23:12:00 -0600

    primera modificación en front

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   //My RAILWAY 
    
   /*API_URI: 'https://back-v9n1.onrender.com/',
